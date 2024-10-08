2975963d014e9feb37ff09ecdee38e1325d84832
agrim/106 <agrimchaudhary2@gmail.com>
2024-10-08 14:19:13 +0530

    [Agrim] ADD: Added program to print three names

---
+++ PrintThreeNames.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class PrintThreeNames{
+public static void main(String [] args){
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter first name");
+String name1 = sc.nextLine();
+System.out.println("enter second name");
+String name2 = sc.nextLine();
+System.out.println("enter third name");
+String name3 = sc.nextLine();
+System.out.println(name1);
+System.out.println(name2);
+System.out.println(name3);
+	}
+}
+
