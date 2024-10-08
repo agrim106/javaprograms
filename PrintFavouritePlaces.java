9d3019be0fea8975a90d289a0269274fd5bfb018
agrim/106 <agrimchaudhary2@gmail.com>
2024-10-08 14:51:19 +0530

    [Agrim] ADD: Added another java code to the repo

---
+++ PrintFavouritePlaces.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class PrintFavouritePlaces{
+public static void main(String [] args){
+Scanner sc  = new Scanner(System.in);
+System.out.println("write a place");
+String place1 = sc.nextLine();
+System.out.println("Write another place");
+String place2 = sc.nextLine();
+System.out.println("Write a third place");
+String place3 = sc.nextLine();
+System.out.println("Write a fourth place");
+String place4 = sc.nextLine();
+System.out.println("write a fifth place");
+String place5 = sc.nextLine();
+System.out.println(place1);
+System.out.println(place2);
+System.out.println(place3);
+System.out.println(place4);
+System.out.println(place5);
+	}
+ }
